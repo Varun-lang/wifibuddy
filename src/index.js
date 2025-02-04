22acc594e549c7475fb9b88040f5faac0e3f9bf3
Varun-lang <varun2001guptaa@gmail.com>
2025-02-04 21:36:49 +0530

    commitv3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Home from "./components/Home";
 import App from "./App";
 import { Toaster } from "react-hot-toast";
 
